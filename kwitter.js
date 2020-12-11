da887e48dbe3a242da589f197b320912f1005ef6
PRATYUSH <pratyushroy.whj@gmail.com>
2020-12-11 22:39:50 +0530

    first commit

---
+++ kwitter.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    var firebaseConfig = {
+        apiKey: "AIzaSyC7NKPbLgUYgtwCKexC97I9f-5GNzhrrfI",
+        authDomain: "tkwitter-66728.firebaseapp.com",
+        projectId: "tkwitter-66728",
+        storageBucket: "tkwitter-66728.appspot.com",
+        messagingSenderId: "1090902628477",
+        appId: "1:1090902628477:web:0b809acd97a080605a7914",
+        measurementId: "G-8R88Q1EC8F"
+    };
+
+    firebase.initializeApp(firebaseConfig);
+}
